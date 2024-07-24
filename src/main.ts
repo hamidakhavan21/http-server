b93bac16a701bb6f29fdd4c2e6019fd7ddc3d7dc
21eich <hawmidak@gmail.com>
2024-07-24 12:58:46 +0330

    user login

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import http, { createServer, get } from "http"
 import Express, { query } from "express"  
-import { title } from "process";
+
+type userRole = "Admin" | "Rep" | "normal"
+
+interface user {
+    id: string,
+    username: string,
+    password: string,
+    role : userRole
+}
+
+
 
 interface plan {
     id: number,
@@ -10,15 +19,52 @@ interface plan {
 
 const plans : plan [] = []
 
+const users : user[] = [
+    {
+        id: "1",
+        username:"admin",
+        password:"admin",
+        role:"Admin"
+    }
+]
+
 const app = Express()
 
-
-
 app.use(Express.json());
+
 app.use((req,res,next)=>{
     console.log(req.method,req.url)
     next()
 })
+
+app.post("/login",(req,res)=>{
+    const {username,password} =req.body;
+    
+    if(
+        username == undefined || 
+        typeof(username) != "string" && username.length == 0        
+    ){
+    res.status(400).send({message: "wrong Usename format"})
+    return;
+    }
+    if(
+        password == undefined || 
+        typeof(password) != "string" && password.length == 0        
+    ){
+    res.status(400).send({message: "wrong format password"})
+    return
+    }
+
+    const user = users.find(
+        (x) => x.username == username && x.password == password
+    );
+
+    if(user == undefined){
+        res.status(401).send({message: "invalid username or password"})
+    }
+
+    res.status(200).send(user);
+});
 
 app.post("/plan",(req,res)=>{
     const { title, description } = req.body
